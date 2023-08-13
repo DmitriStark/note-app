623e796896c36876d3993ececff843b120dc1ce9
aw3s0m3ss <aw3s0m3ss@gmail.com>
2023-08-13 20:38:29 +0300

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://aw3s0m3ss.github.io/note-app/",
+  base: "/note-app/",
   plugins: [react()],
 })
