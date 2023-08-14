d5a50d3ec4ece5f13f4b1babad6fc19b88c233ac
aw3s0m3ss <aw3s0m3ss@gmail.com>
2023-08-14 21:59:51 +0300

    path link changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,55 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-import { Routes, Route , Navigate} from "react-router-dom" ;
+import { Routes, Route, Navigate } from "react-router-dom";
 import { NewNote } from "./NewNote";
 import { useLocalStorage } from "./useLocalStorage";
 import { useMemo } from "react";
-import {v4 as uuidv4} from "uuid"
+import { v4 as uuidv4 } from "uuid"
 import { NoteList } from "./NoteList";
 import { NoteLayout } from "./NoteLayout";
-import {Note} from "./Note.tsx";
-import {EditNote} from "./editNote.tsx";
+import { Note } from "./Note.tsx";
+import { EditNote } from "./editNote.tsx";
 
 
-export type Note ={
-  id:string
+export type Note = {
+  id: string
 }
-&NoteData
+  & NoteData
 export type RawNote = {
-  id:string
+  id: string
 }
-&RawNoteData
+  & RawNoteData
 
 export type RawNoteData = {
-  title:string
-  markdown:string
-  tagIds:string[]
+  title: string
+  markdown: string
+  tagIds: string[]
 }
 export type NoteData = {
-  title:string
-  markdown:string
-  tags:Tag[]
+  title: string
+  markdown: string
+  tags: Tag[]
 }
 
-export type Tag ={
-  id:string
-  label:string
+export type Tag = {
+  id: string
+  label: string
 }
 
 
-function App(){
-  const [notes , setNotes] =useLocalStorage<RawNote[]>("NOTES",[])
-  const [tags , setTags] =useLocalStorage<Tag[]>("TAGS",[])
+function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
 
   const notesWithTags = useMemo(() => {
     return notes.map(note => {
       return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
     })
   }, [notes, tags])
-  function onCreateNote({tags,...data}:NoteData){
-    setNotes(prevNotes=>{
-      return [...prevNotes,{...data , id:uuidv4(),tagIds : tags.map(tag=>tag.id)}]
-    })    
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [...prevNotes, { ...data, id: uuidv4(), tagIds: tags.map(tag => tag.id) }]
+    })
   }
 
   function onUpdateNote(id: string, { tags, ...data }: NoteData) {
@@ -64,49 +64,49 @@ function App(){
     })
   }
 
-  function onDeleteNote(id:string){
-    setNotes(prevNotes=>{
-      return prevNotes.filter(note=> note.id !== id)
+  function onDeleteNote(id: string) {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
     })
 
   }
 
-  function addTag(tag:Tag){
-    setTags(prev=> [...prev,tag])
+  function addTag(tag: Tag) {
+    setTags(prev => [...prev, tag])
   }
 
-  function updateTag(id:string,label:string){
+  function updateTag(id: string, label: string) {
     setTags(prevTags => {
-      return prevTags.map(tag=>{
-        if(tag.id === id){
-          return {...tag,label}
-        }else{
+      return prevTags.map(tag => {
+        if (tag.id === id) {
+          return { ...tag, label }
+        } else {
           return tag
         }
       })
     })
   }
 
-  function deleteTag(id:string){
-    setTags(prevTags=>{
-      return prevTags.filter(tag=> tag.id !== id)
+  function deleteTag(id: string) {
+    setTags(prevTags => {
+      return prevTags.filter(tag => tag.id !== id)
     })
 
   }
 
   return (
     <Container className="my-4">
-  <Routes>
-  <Route path ="https://aw3s0m3ss.github.io/note-app/" element = {<NoteList notes={notesWithTags} availableTags={tags} onUpdateTag ={updateTag} onDeleteTag = {deleteTag}/>}/>
-  <Route path ="/new" element = {<NewNote onSubmit ={onCreateNote} onAddTag ={addTag} availibleTags = {tags}/>}/>
-  <Route path=":id" element={<NoteLayout notes = {notesWithTags}/>}>
-    <Route index element = {<Note onDelete = {onDeleteNote}/>}/>
-    <Route path="edit" element = {<EditNote onSubmit ={onUpdateNote} onAddTag ={addTag} availibleTags = {tags}/>}/>
-    
-  </Route>
-  <Route path ="*" element = {<Navigate to = "https://aw3s0m3ss.github.io/note-app/"/>}/>
-  </Routes>
-  </Container>
+      <Routes>
+        <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags} onUpdateTag={updateTag} onDeleteTag={deleteTag} />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availibleTags={tags} />} />
+        <Route path=":id" element={<NoteLayout notes={notesWithTags} />}>
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route path="edit" element={<EditNote onSubmit={onUpdateNote} onAddTag={addTag} availibleTags={tags} />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
   )
 }
+
 export default App
